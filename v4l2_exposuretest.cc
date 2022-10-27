6287becac97333ca79b2afb69f6994d00915610e
Yuto Nakamura <maron07222010@gmail.com>
2022-10-27 22:03:00 +0900

    完成

--- v4l2_exposuretest.cc
+++ v4l2_exposuretest.cc
@@ -21,8 +21,8 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 
-#define WIDTH 1600
-#define HEIGHT 1200
+#define WIDTH 1280
+#define HEIGHT 720
 
 struct buffer {
     void* start;
@@ -43,6 +43,109 @@ struct buffer *buffers;
 int fd;
 /*the number of buffers*/
 int n_buffers;
+/*for auto exposure*/
+float cur_ev[3] = {1000.0,1000.0,1000.0};
+int frame = 0;
+float target_grey_fraction = 0.3;
+int now_gain = 1;
+float min_ev = 5 * 1.0;
+float max_ev = 500 * 500.0;
+
+float set_exposure_target(cv::Mat b, int x_start, int x_end, int x_skip, int y_start, int y_end, int y_skip) {
+    int lum_med;
+    uint32_t lum_binning[256] = {0};
+
+    unsigned int lum_total = 0;
+    for (int y = y_start; y < y_end; y += y_skip) {
+        for (int x = x_start; x < x_end; x += x_skip) {
+        uint8_t lum = b.data[y * WIDTH + x];
+        lum_binning[lum]++;
+        lum_total += 1;
+        }
+    }
+
+    // Find mean lumimance value
+    unsigned int lum_cur = 0;
+    for (lum_med = 255; lum_med >= 0; lum_med--) {
+    lum_cur += lum_binning[lum_med];
+        if (lum_cur >= lum_total / 2) {
+            break;
+        }
+    }
+
+    return lum_med / 256.0;
+}
+
+void camera_autoexposure(float grey_frac) {
+  const float dt = 0.05;
+
+  const float ts_grey = 10.0;
+  const float ts_ev = 0.05;
+
+  const float k_grey = (dt / ts_grey) / (1.0 + dt / ts_grey);
+  const float k_ev = (dt / ts_ev) / (1.0 + dt / ts_ev);
+
+  const float cur_ev_ = cur_ev[frame % 3];
+
+  float new_target_grey = std::max(0.1, std::min(0.9 - 0.4 * log2(1 + cur_ev_) / log2(6000.0), 0.9));
+  float target_grey = (1 - k_grey) * target_grey_fraction + k_grey * new_target_grey;
+
+  float desired_ev = std::max(min_ev , std::min(cur_ev_ * target_grey / grey_frac,  max_ev));
+  float k = (1.0 - k_ev) / 3.0;
+  desired_ev = (k * cur_ev[0]) + (k * cur_ev[1]) + (k * cur_ev[2]) + (k_ev * desired_ev);
+
+  float best_ev_score = 1e6;
+  int new_g = 0;
+  int new_t = 0;
+  printf("mean grey = %f, target_gery = %f, desired_ev = %f\n", grey_frac, target_grey, desired_ev);
+
+  for (int gain = std::max(0, now_gain - 5); gain <= std::min(30, now_gain + 5); gain++) {
+    int temp = std::min((int)desired_ev/(gain+10), 500);
+    int t = std::max(1, temp);
+
+    float score = std::abs(desired_ev - (t * (gain + 10))) * 10;
+    float score2 = std::abs(desired_ev - ((t+1) * (gain+10)))*10;
+    if (score > score2){
+      score = score2;
+      t++;
+    }
+
+    float m;
+    if (gain > 20 ) {
+      m = 20.0;
+    } else if (gain > 10 ) {
+      m = 20.0;
+    } else if (gain > 0 ) {
+      m = 20.0;
+    } else {
+      m = 1.0;
+    }
+    // 0 is desired gain
+    score += gain * m;
+    score += std::abs(gain - now_gain) * (score + 1.0) / 10.0;
+
+    if (score < best_ev_score) {
+      new_t = t;
+      new_g = gain;
+      best_ev_score = score;
+    }
+  }
+  cur_ev[frame % 3] = (new_g + 10) * new_t;
+  target_grey_fraction = target_grey;
+  now_gain = new_g;
+  printf("new_gain = %d, new_exposure_time = %d ms\n", new_g, new_t / 10);
+  struct v4l2_control ctrl;
+  ctrl.id = V4L2_CID_EXPOSURE_AUTO;
+  ctrl.value = 1;
+  ioctl(fd, VIDIOC_S_CTRL, &ctrl);
+  ctrl.id = V4L2_CID_EXPOSURE_ABSOLUTE;
+  ctrl.value = new_t;
+  ioctl(fd, VIDIOC_S_CTRL, &ctrl);
+  ctrl.id = V4L2_CID_GAIN;
+  ctrl.value = new_g;
+  ioctl(fd, VIDIOC_S_CTRL, &ctrl);
+}
+
 
 void open_device(){
     fd = open("/dev/video0", O_RDWR);
@@ -66,7 +169,17 @@ void cap_device(){
 
 }
 
-void set_device(){
+void set_device(){ 
+    struct v4l2_control ctrl;
+    ctrl.id = V4L2_CID_EXPOSURE_AUTO;
+    ctrl.value = 1;
+    ioctl(fd, VIDIOC_S_CTRL, &ctrl);
+    ctrl.id = V4L2_CID_EXPOSURE_ABSOLUTE;
+    ctrl.value = 100;
+    ioctl(fd, VIDIOC_S_CTRL, &ctrl);
+    ctrl.id = V4L2_CID_GAIN;
+    ctrl.value = 1;
+    ioctl(fd, VIDIOC_S_CTRL, &ctrl);
     struct v4l2_format fmt = {0};
     fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     fmt.fmt.pix.width = WIDTH;
@@ -86,6 +199,7 @@ void set_device(){
     }
     if (fmt.fmt.pix.width != WIDTH || fmt.fmt.pix.height != HEIGHT || fmt.fmt.pix.pixelformat != V4L2_PIX_FMT_MJPEG){
         printf("The desired format is not supported\n");
+        printf("%dx%d\n", fmt.fmt.pix.width, fmt.fmt.pix.height);
     }
 }
 
@@ -167,6 +281,9 @@ int make_image(){
     cv::InputArray hoge(src);
     cv::Mat decodedImage = cv::imdecode(hoge,cv::IMREAD_ANYCOLOR);
     cv::imshow("test",decodedImage);
+    cv::Mat grey;
+    cv::cvtColor(decodedImage, grey, cv::COLOR_RGB2GRAY);
+    camera_autoexposure(set_exposure_target(grey, 0, 1280, 2, 100, 600, 2));
     /*next enqueue index return*/
     return buf.index;
 }
@@ -201,14 +318,15 @@ int main(){
     enqueue_buffers();
     stream_start();
     while(true){
-    	int enqueue_index = make_image();
-   	 if (enqueue_index == -1){
+        int enqueue_index = make_image();
+   	    if (enqueue_index == -1){
             return EXIT_FAILURE;
-    	 }
+    	}
     	enqueue_buffer(enqueue_index);
-	if (cv::waitKey(10)!=-1){
-		break;
-	}
+	    if (cv::waitKey(10)!=-1){
+		    break;
+	    }
+        frame++;
     }
     stream_stop();
     munmap_buffer();
